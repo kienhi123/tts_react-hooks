f9783fabac405f9c281da03570a2c5cd89b04665
kienhi123 <kien123@gmail.com>
2022-09-06 09:39:39 +0700

    update:todoList

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 
+import { useState } from 'react';
 import './App.scss';
-import ColorBox from './components/ColorBox';
+import Todolist from './components/Todolist';
 
 function App() {
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: "Trần Văn Kiên" },
+    { id: 2, title: "Hello world" },
+    { id: 3, title: "Frontend" },
+    { id: 4, title: "Backend" },
+  ]);
+  function handleTodoClick(todo) {
+    console.log(todo)
+    const index = todoList.findIndex(item => item.id = todo.id)
+    if (index < 0) return
+
+    const newTodoList = [...todoList]
+    newTodoList.splice(index, 1)
+    setTodoList(newTodoList)
+
+
+  }
+
   return (
     <div className="App">
       <h1>
-        Welcome to React Hooks !
+        React hooks - TodoList
       </h1>
-      <ColorBox />
+      <Todolist todos={todoList} onTodoClick={handleTodoClick} />
     </div>
   );
 }
